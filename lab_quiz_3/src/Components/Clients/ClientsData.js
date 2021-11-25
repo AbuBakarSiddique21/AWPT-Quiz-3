dd9ac8b77964e74db42449fffeb8f8b8b3fbd10e
Abu Bakar Siddique <1993siddique3@gmail.com>
2021-11-25 22:58:36 +0600

    lab_quiz_3_complete

---
+++ lab_quiz_3/src/Components/Clients/ClientsData.js
@@ -0,0 +1,34 @@
+import client1 from '../images/clients/client-1.png'
+import client2 from '../images/clients/client-2.png'
+import client3 from '../images/clients/client-3.png'
+import client4 from '../images/clients/client-4.png'
+import client5 from '../images/clients/client-5.png'
+import client6 from '../images/clients/client-6.png'
+
+const ClientsData = [
+    {
+        id: 1,
+        image: client1
+    },
+    {
+        id: 2,
+        image: client2
+    },
+    {
+        id: 3,
+        image: client3
+    },
+    {
+        id: 4,
+        image: client4
+    },
+    {
+        id: 5,
+        image: client5
+    },
+    {
+        id: 6,
+        image: client6
+    }
+]
+export default ClientsData;
